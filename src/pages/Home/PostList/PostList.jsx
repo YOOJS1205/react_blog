9d1bbfa84f204d73f7e6229a8a074397206d7097
YOOJS1205 <junsang8921@gmail.com>
2022-06-20 16:52:18 +0900

    post 데이터 axios로 처리

--- src/pages/Home/PostList/PostList.jsx
+++ src/pages/Home/PostList/PostList.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Post from '../Post/Post';
 import './postList.css';
-import {data} from '../../../data';
 
 
 const PostList = () => {
-    return (
-        <ul className='posts'>
-			{data.posts.map(item => (
-				<Post
-				key={item.id}
-				thumbnailSrc={item.thumbnail}
-				title={item.title}
-				profileSrc={item.profileImg}
-				created={item.created}/>
-			))}
-		</ul>
-    )
+	// json 데이터 담는 변수 초기 설정
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [postObj, setPostObj] = useState({});
+
+	// 마운트될 때 포스트 데이터 postObj 변수에 담기
+	useEffect(() => {
+		async function getData() {
+			const res = await axios.get('/assets/data.json');
+			const result = await res.data.posts;
+			setPostObj(result);
+			setIsLoaded(true);
+		}
+		getData();
+		// axios.get('/assets/data.json')
+		// 	.then(response => setPostObj(response.data.posts));
+	}, [])
+
+	if (isLoaded) {
+		return (
+			<ul className='posts'>
+				{postObj.map(item => (
+					<Post
+					key={item.id}
+					thumbnailSrc={item.thumbnail}
+					title={item.title}
+					profileSrc={item.profileImg}
+					created={item.created}/>
+				))}
+			</ul>
+		)
+	}
 }
 
 export default PostList;
